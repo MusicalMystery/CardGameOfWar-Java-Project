3bc1a11ce3bd4a70563e9ee5c4a4c9b3a5ab063a
Peter Loban <114698853+MusicalMystery@users.noreply.github.com>
2022-10-24 15:39:51 -0400

    Update MainGame.java

--- gameIdea/src/gameIdea/MainGame.java
+++ gameIdea/src/gameIdea/MainGame.java
@@ -11,56 +11,56 @@ final int deckSize = 52;
 
 	public static void main(String[] args) {
 		
-		//Initialize Game here
+	//Initialize Game here
 		
 		Deck mainDeck = new Deck();
 		
-		//Shuffle the deck
+	//Shuffle the deck
 		
 		mainDeck.shuffleDeck();
 		
-		//Split the deck
+	//Split the deck
 		
 		mainDeck.splitDeck();
 		
-		//Create Players and give them each one half of the shuffled deck
+	//Create Players and give them each one half of the shuffled deck
 		
 		Player player1 = new Player();
 		Player player2 = new Player();
 		player1.giveCards(mainDeck.pOneHalf);
 		player2.giveCards(mainDeck.pTwoHalf);
 		
-		//Create a scanner to input the players' names
+	//Create a scanner to input the players' names
 		
 		Scanner reader = new Scanner(System.in);
 		
 		System.out.print("Player 1 type your name and press Enter: ");
 		
 		player1.name = reader.nextLine();
-		//player1.name = "PlayerOne";
+	//player1.name = "PlayerOne";
 		
 		System.out.print("Player 2 type your name and press Enter: ");
 		
 		player2.name = reader.nextLine();
-		//player2.name = "PlayerTwo";
+	//player2.name = "PlayerTwo";
 		
-		//Create an announcement for the beginning of the game
+	//Create an announcement for the beginning of the game
 		
 		System.out.println();
 		System.out.println(player1.name + " versus " + player2.name);
 		System.out.println("Let the game begin!");
 		System.out.println("(Press 'y' followed by 'Enter' to play rounds)");
 		
-		//this String "y" is simply the "y" that has to be typed to play each round of the game
+	//this String "y" is simply the "y" that has to be typed to play each round of the game
 		String y = null;
 		
 		
-		//The main bulk of the game is here, encapsulated in a while loop that will repeat until one player is detected as a winner
+	//The main bulk of the game is here, encapsulated in a while loop that will repeat until one player is detected as a winner
 		
 		while (player1.winner == false && player2.winner == false) {
 		
 			
-			//The player types in "y" and then "Enter" and the program compares the card in the 0th spot of each players ownedCards
+	//The player types in "y" and then "Enter" and the program compares the card in the 0th spot of each players ownedCards
 			
 			y = reader.nextLine();
 			
@@ -74,7 +74,7 @@ final int deckSize = 52;
 			player2.ownedCards.get(0).printCard();
 			
 			
-			//if player1 wins (Meaning their 0th of ownedCards is higher than player2's) then do this
+	//if player1 wins (Meaning their 0th of ownedCards is higher than player2's) then do this
 			
 			if (player1.drawTopCard().value > player2.drawTopCard().value) {
 				
@@ -89,7 +89,7 @@ final int deckSize = 52;
 			
 			} else 
 				
-			//if player2 wins (Meaning their 0th of ownedCards is higher than player1's) then do this
+	//if player2 wins (Meaning their 0th of ownedCards is higher than player1's) then do this
 				
 				if (player1.drawTopCard().value < player2.drawTopCard().value) {
 				
@@ -101,7 +101,7 @@ final int deckSize = 52;
 				
 				System.out.println(player2.name + " wins the round!");
 
-			//if there's a tie then do this
+	//if there's a tie then do this
 				
 			} else if (player1.drawTopCard().value == player2.drawTopCard().value) {
 				
@@ -127,7 +127,7 @@ final int deckSize = 52;
 				System.out.print(player2.name + " drew a ");
 				player2.ownedCards.get(i).printCard();
 				
-				//if there's another tie then do this
+	//if there's another tie then do this
 				
 				while (player1.ownedCards.get(i).value == player2.ownedCards.get(i).value) { //Had a trouble spot here where I had to write code to fix an exception when a player has no more cards but is faced with a tie
 					
@@ -184,9 +184,9 @@ final int deckSize = 52;
 				} else
 				
 				
-				//beyond this point the cards are different values and now the winner just needs to take all the cards that were drawn
+	//beyond this point the cards are different values and now the winner just needs to take all the cards that were drawn
 				
-				//if player1 wins
+	//if player1 wins
 				if (player1.ownedCards.get(i).value > player2.ownedCards.get(i).value) {
 					
 				for (int j = i; j >=0; j--) {
@@ -202,7 +202,7 @@ final int deckSize = 52;
 				System.out.println(player1.name + " wins the round!");
 
 				
-				//if player2 wins
+	//if player2 wins
 			} else if (player2.ownedCards.get(i).value > player1.ownedCards.get(i).value) {
 				
 			for (int j = i; j >=0; j--) {
@@ -218,12 +218,12 @@ final int deckSize = 52;
 			System.out.println(player2.name + " wins the round!");
 
 			
+				}
 			}
-		}
 			
 		}
 			
-			//This code determines if either player has run out of cards.  If a player has run out of cards, this ends the initial while loop and proceeds to the final step of announcing the winner
+	//This code determines if either player has run out of cards.  If a player has run out of cards, this ends the initial while loop and proceeds to the final step of announcing the winner
 		
 		if (player1.ownedCards.size() == 0) {
 			player2.winner = true;
@@ -231,14 +231,12 @@ final int deckSize = 52;
 			player1.winner = true;
 		}
 			
-		//this code runs if neither player has won yet, and simply states how many cards each player has and then restarts the initial while loop to start the next round
+	//this code runs if neither player has won yet, and simply states how many cards each player has and then restarts the initial while loop to start the next round
 		
 		System.out.println();
 		System.out.println(player1.name + " has " + player1.ownedCards.size() + " cards");
 		System.out.println(player2.name + " has " + player2.ownedCards.size() + " cards");
 
-		
-		
 		}
 
 		System.out.println();
@@ -251,14 +249,6 @@ final int deckSize = 52;
 			System.out.println(player2.name + " WINS THE GAME!");
 		} 
 
-
-
-		
-		
-		
-		
-
-		
 	}
 
 }
